de8be6257ac98859fbb3069e7876265a28121426
Grosu Igor <igrosu@griddynamics.com>
2026-06-17 20:14:28 +0300

    feat(store): created global store
    Unifies global providers (all except query)

---
+++ frontend/src/store/index.ts
@@ -0,0 +1,9 @@
+export { useCartContext, useCartDispatchContext } from "./items/cart";
+export { GlobalProvider } from "./global";
+export { useCurrencyContext, useLocaleContext } from "./preferences";
+export {
+  QueryProvider,
+  useQueryContext,
+  useQueryDispatchContext,
+} from "./query";
+export { useWishlistContext, useWishlistDispatchContext } from "./items/wishlist";
